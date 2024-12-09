34cffdc1836c92167272587eb0ea65466762c0bf
Kim Minchan <galaxyeyes02@naver.com>
2024-12-09 22:12:24 +0900

    fix : get user_id, menu_id, order_id

--- menu_server.py
+++ menu_server.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from sqlalchemy.orm import Session
 from model import ReviewTable, UserTable, StoreTable, MenuTable, OrderTable
 from db import session
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 import os
 import uvicorn
 
@@ -16,6 +17,7 @@ menu.add_middleware(
     allow_headers=["*"],
 )
 
+menu.add_middleware(SessionMiddleware, secret_key="your-secret-key")
 
 def get_db():
     db = session()
@@ -23,6 +25,22 @@ def get_db():
         yield db
     finally:
         db.close()
+
+#로그인 상태 확인, 로그인 중인 유저 아이디 반환
+@menu.get("/check_login/")
+async def check_login(request: Request):
+    # 세션에서 사용자 정보 확인
+    if "user_id" not in request.session:
+        return False
+    
+    return {"user_id": f"{request.session['user_id']}"}
+
+#가게 아이디로 가게의 모든 메뉴 아이디들을 반환
+@menu.get("/menu_ids/{store_id}")
+async def get_menu_ids(store_id: int, db: Session = Depends(get_db)):
+    menu_ids = db.query(MenuTable.menu_id).filter(MenuTable.store_id == store_id).all()
+    
+    return {"menu_ids": [menu_id[0] for menu_id in menu_ids]}
 
 #메뉴 이름, 메뉴 설명, 메뉴 사진 딕셔너리 반환(메인)
 @menu.get("/main_menu/{menu_id}")
@@ -64,6 +82,7 @@ async def increase_order_quantity(user_id: str, menu_id: int, store_id: int, db:
         order.quantity += 1
         db.commit()
         db.refresh(order)
+        return {"order_id": order.order_id}
 
     else:
         new_order = OrderTable(
@@ -76,6 +95,7 @@ async def increase_order_quantity(user_id: str, menu_id: int, store_id: int, db:
         db.add(new_order)
         db.commit()
         db.refresh(new_order)
+        return {"order_id": new_order.order_id}
 
 if __name__ == "__menu__":
     port = int(os.environ.get("PORT", 8000))
